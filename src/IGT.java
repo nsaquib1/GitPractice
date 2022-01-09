bc9266b4d1b18f1211a3d5c0f2d2246f0dfc4107
nsaquib1 <97128392+nsaquib1@users.noreply.github.com>
2022-01-09 18:53:13 -0500

    test case 2

--- src/IGT.java
+++ src/IGT.java
@@ -4,6 +4,8 @@ public class IGT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("this is a test case");
+		System.out.println("this is test case 2"); 
+		
 	}
 
 }
